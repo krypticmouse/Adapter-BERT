30f47466f47534bdc520dc943e227d607f395427
“krypticmouse” <herumbshandilya123@gmail.com>
2023-03-16 02:05:03 +0530

    Modularize training

--- train-v2.py
+++ train-v2.py
@@ -5,18 +5,6 @@ from dataset import CoLADataset
 from model import BertClassifier
 from transformers import Trainer, BertTokenizer, DataCollatorWithPadding, TrainingArguments, AdamW
 
-model = BertClassifier(num_labels = 1)
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-layers = ["adapter", "LayerNorm"]
-params = [p for n, p in model.named_parameters() \
-                if any([(nd in n) for nd in layers])]
-optimizer = AdamW(params)
-
-train_dataset = CoLADataset('data/in_domain_train.tsv', tokenizer)
-val_dataset = CoLADataset('data/in_domain_dev.tsv', tokenizer)
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
 class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.get("label")
@@ -28,25 +16,40 @@ class CustomTrainer(Trainer):
         loss = loss_fct(outputs, labels)
         return (loss, outputs) if return_outputs else loss
 
-training_args = TrainingArguments(
-    "adapter-trainer",
-    per_device_train_batch_size = BATCH_SIZE,
-    per_device_eval_batch_size = BATCH_SIZE,
-    evaluation_strategy = 'epoch',
-    learning_rate = LEARNING_RATE,
-    num_train_epochs = EPOCHS,
-    warmup_ratio = 0.1,
-    dataloader_num_workers = 2,
-    dataloader_drop_last = True,
-    seed=42
-)
+def train():
+    model = BertClassifier(num_labels = 1)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-trainer = CustomTrainer(
-    model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=val_dataset,
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-    optimizers = optimizer
-)
+    layers = ["adapter", "LayerNorm"]
+    params = [p for n, p in model.named_parameters() \
+                    if any([(nd in n) for nd in layers])]
+    optimizer = AdamW(params)
+
+    train_dataset = CoLADataset('data/in_domain_train.tsv', tokenizer)
+    val_dataset = CoLADataset('data/in_domain_dev.tsv', tokenizer)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    
+    training_args = TrainingArguments(
+        "adapter-trainer",
+        per_device_train_batch_size = BATCH_SIZE,
+        per_device_eval_batch_size = BATCH_SIZE,
+        evaluation_strategy = 'epoch',
+        learning_rate = LEARNING_RATE,
+        num_train_epochs = EPOCHS,
+        warmup_ratio = 0.1,
+        dataloader_num_workers = 2,
+        dataloader_drop_last = True,
+        seed=42
+    )
+
+    trainer = CustomTrainer(
+        model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        optimizers = optimizer
+    )
+
+    trainer.train()
